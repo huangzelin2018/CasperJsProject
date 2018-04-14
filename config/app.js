35b8a7bdd929dca1d1818564bb0dda66e52b0227
huangzl <huang_ze_lin@163.com>
2018-04-15 01:43:10 +0800

    update app.js

--- config/app.js
+++ config/app.js
@@ -2,8 +2,7 @@ const date = require('../tool/DateUtil');
 const util = require('util');
 var cfg = {
     htmlPath: function (code) {
-        var filePath = "html/" + code + ".html";
-        return filePath;
+        return util.format('html/%s.html', code);
     },
     logPath: function () {
         return util.format('log/error_%s.log', date.getDate('yyyy-MM-dd'));
